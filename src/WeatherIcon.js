e8d934b2c8b8dd16a2e047ee6d7827c70fcfbd63
ShimaTarighat <127606202+ShimaTarighat@users.noreply.github.com>
2023-06-10 22:06:44 +0330

    Set degree

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -13,10 +13,12 @@ export default function WeatherIcon(props) {
     "04n": "CLOUDY",
     "09d": "RAIN",
     "09n": "RAIN",
-
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "RAIN",
+    "11n": "RAIN",
     "13d": "SNOW",
     "13n": "SNOW",
-
     "50d": "FOG",
     "50n": "FOG",
   };
