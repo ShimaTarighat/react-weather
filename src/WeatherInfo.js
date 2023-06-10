78541a5bba78d7731aac1a276fde850faae92999
ShimaTarighat <127606202+ShimaTarighat@users.noreply.github.com>
2023-06-10 17:22:09 +0330

    Added WeatherInfo

---
+++ src/WeatherInfo.js
@@ -0,0 +1,34 @@
+import React from "react";
+import WeatherInfo from "./WeatherInfo";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <formattedDate date={props.data.date} />
+        </li>
+        <li>{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} size={50} />
+            </div>
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity} %</li>
+            <li>Wind: {props.data.wind} km/h</li>{" "}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
